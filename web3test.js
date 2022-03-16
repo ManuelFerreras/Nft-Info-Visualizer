29bd8737d2a3fe8428261c98e9ac4a6a44a571ba
ManuelFerreras <manuel.ferreras1@gmail.com>
2022-03-16 08:51:27 -0300

    erc1155

--- web3test.js
+++ web3test.js
@@ -9,7 +9,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(ethNetwork));
 var ens = new ENS(web3);
 
 const ERC721Abi = [{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ApprovalCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"ApprovalQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"ApprovalToCurrentOwner","type":"error"},{"inputs":[],"name":"ApproveToCaller","type":"error"},{"inputs":[],"name":"BalanceQueryForZeroAddress","type":"error"},{"inputs":[],"name":"MintToZeroAddress","type":"error"},{"inputs":[],"name":"MintZeroQuantity","type":"error"},{"inputs":[],"name":"OwnerIndexOutOfBounds","type":"error"},{"inputs":[],"name":"OwnerQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"TokenIndexOutOfBounds","type":"error"},{"inputs":[],"name":"TransferCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"TransferFromIncorrectOwner","type":"error"},{"inputs":[],"name":"TransferToNonERC721ReceiverImplementer","type":"error"},{"inputs":[],"name":"TransferToZeroAddress","type":"error"},{"inputs":[],"name":"URIQueryForNonexistentToken","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"totalMinted","type":"uint256"}],"name":"Minted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"MAX_SUPPLY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newPrice","type":"uint256"}],"name":"changePrice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"flipSale","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_count","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"saleOpen","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}];
-
+const ERC1155Abi = [{"inputs":[{"internalType":"string","name":"uri_","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256[]","name":"ids","type":"uint256[]"},{"indexed":false,"internalType":"uint256[]","name":"values","type":"uint256[]"}],"name":"TransferBatch","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"id","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"TransferSingle","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"value","type":"string"},{"indexed":true,"internalType":"uint256","name":"id","type":"uint256"}],"name":"URI","type":"event"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address[]","name":"accounts","type":"address[]"},{"internalType":"uint256[]","name":"ids","type":"uint256[]"}],"name":"balanceOfBatch","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256[]","name":"ids","type":"uint256[]"},{"internalType":"uint256[]","name":"amounts","type":"uint256[]"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeBatchTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"uri","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}];
 
 // Snippets
 async function getContractCreator(collectionAddress) {
@@ -117,16 +117,28 @@ async function checkENSName(address) {
     });
 }
 
-async function checkMetadata(contract, tokenId) {
-    return await contract.methods.tokenURI(tokenId).call().then(res => {
-        if(res == "") {
-            return "";
-        } else {
-            return(res);
-        }
-    }).catch(() => {
-        console.log("Metadata ERROR: Non Existent Token Id");
-    });
+async function checkMetadata(contract, tokenId, type) {
+    if (type) {
+        return await contract.methods.tokenURI(tokenId).call().then(res => {
+            if(res == "") {
+                return "";
+            } else {
+                return(res);
+            }
+        }).catch(() => {
+            console.log("Metadata ERROR: Non Existent Token Id");
+        });
+    } else if (!type) {
+        return await contract.methods.uri(tokenId).call().then(res => {
+            if(res == "") {
+                return "";
+            } else {
+                return(res);
+            }
+        }).catch(() => {
+            console.log("Metadata ERROR: Non Existent Token Id");
+        });
+    }   
 }
 
 async function checkMetadataFields(metadataURL) {
@@ -136,7 +148,7 @@ async function checkMetadataFields(metadataURL) {
         } else {
             console.log("Incomplete Metadata");
         }
-    });
+    }).catch(err => console.log("Meta Fields Error"));
 }
 
 async function checkIfIPFSMetadata(metadataURL) {
@@ -166,7 +178,7 @@ async function checkIfAudited(contractAddress) {
                 return true;
             }
         });
-    });
+    }).catch(err => console.log("Audit Error"));
 }
 
 async function checkIfImageAvailable(metadataURL) {
@@ -183,7 +195,7 @@ async function checkIfImageAvailable(metadataURL) {
             console.log("Metadata Does Not Contain an Image");
             return false;
         }
-    });
+    }).catch(err => console.log("Image Availability Error"));
 }
 
 async function checkIfImageIsIPFS(metadataURL) {
@@ -200,7 +212,7 @@ async function checkIfImageIsIPFS(metadataURL) {
             console.log("Metadata Does Not Contain an Image For IPFS Check");
             return false;
         }
-    });
+    }).catch(err => console.log("Image IPFS Error"));
 }
 
 async function getNftName(metadataURL) {
@@ -212,7 +224,7 @@ async function getNftName(metadataURL) {
             console.log("Metadata Does Not Contain a Name")
             return "";
         }
-    });
+    }).catch(err => console.log("Name Error"));
 }
 
 async function checkIfImageIsSSL(metadataURL) {
@@ -229,7 +241,7 @@ async function checkIfImageIsSSL(metadataURL) {
             console.log("Metadata Does Not Contain an Image For SSL Check");
             return false;
         }
-    });
+    }).catch(err => console.log("Image SSL Error"));
 }
 
 function timeConverter(UNIX_timestamp){
@@ -265,8 +277,10 @@ async function getNftInfoByCollectionAndId(collectionAddress, id) {
     let metaImgAvailable;
     let metaImgIPFS;
     let metaImgSSL;
+    let obj;
 
     const contract = new web3.eth.Contract(ERC721Abi, collectionAddress);
+    const erc1155contract = new web3.eth.Contract(ERC1155Abi, collectionAddress);
     
     await checkIfOpenSource(collectionAddress).then(res => {
         opensource = res;
@@ -295,13 +309,36 @@ async function getNftInfoByCollectionAndId(collectionAddress, id) {
         optimizedContract = res;
     });
 
-    await checkMetadata(contract, id).then(res => metaUrl = res);
-
-    if(metaUrl.startsWith("ipfs://")) {
+    if(erc721compliant) {
+        await checkMetadata(contract, id, true).then(res => metaUrl = res).catch(console.log);
+    } else if (erc1155compliant) {
+        await checkMetadata(erc1155contract, id, false).then(res => metaUrl = res).catch(console.log);
+    }
+    if(metaUrl != undefined && metaUrl != "" && metaUrl.startsWith("ipfs://ipfs/")) {
+        metaUrl = "https://ipfs.io/ipfs/" + metaUrl.split("ipfs://ipfs/").pop();
+    } else if(metaUrl != undefined && metaUrl != "" && metaUrl.startsWith("ipfs://")) {
         metaUrl = "https://ipfs.io/ipfs/" + metaUrl.split("ipfs://").pop();
     }
 
     console.log("Metadata: " + metaUrl);
+
+    if(erc721compliant) {
+        contractType = "ERC-721";
+        await getNftOwner(contract, id).then(res => checkENSName(res).then(res => nftOwner = res));
+        await getTokenMintAddress(collectionAddress, id).then(res => checkENSName(res).then(res => nftMinter = res));
+
+        await getLastTransferTimestamp(collectionAddress, id).then(res => {
+            nftLastTransferDate = timeConverter(res);
+        });
+
+        await getTokenMintTimestamp(collectionAddress, id).then(res => {
+            nftMintDate = timeConverter(res);
+        });
+    } else if (erc1155compliant) {
+        contractType = "ERC-1155";
+    } else {
+        contractType = "Other";
+    }
 
     if(metaUrl != undefined && metaUrl != "" && metaUrl.startsWith("http")) {
         await checkMetadataFields(metaUrl).then(res => metaFieldsStandard = res);
@@ -317,52 +354,50 @@ async function getNftInfoByCollectionAndId(collectionAddress, id) {
             metaImgSSL = false;
         }
 
-        await getNftName(metaUrl).then(res => nftName = res);
-        await getNftOwner(contract, id).then(res => checkENSName(res).then(res => nftOwner = res));
-        await getTokenMintAddress(collectionAddress, id).then(res => checkENSName(res).then(res => nftMinter = res));
-        await checkIfComplies(contract).then(res => {
-            if(res[0] == true) {
-                contractType = "ERC-721";
-            } else {
-                contractType = "ERC-1155";
-            }
-        });
-
-        await getLastTransferTimestamp(collectionAddress, id).then(res => {
-            nftLastTransferDate = timeConverter(res);
-        });
-
-        await getTokenMintTimestamp(collectionAddress, id).then(res => {
-            nftMintDate = timeConverter(res);
-        })
-
-
+        await getNftName(metaUrl).then(res => nftName = res);  
     }
 
-
-    const obj = {
-        "nft": {
-            "id": 1, // Done
-            "name": nftName, // Done
-            "created_by": nftMinter, // Done
-            "contract_address": collectionAddress, // Done
-            "token_id": id, // Done
-            "chain_id": 1, // Done
-            "mint_timestamp": nftMintDate, // Done
-            "token_type": contractType, // Done
-            "edition_name": 0, // Done
-            "collection_size": 0, // Done
-            "image_url": metaImgAvailable, // Done
-            "owner": {
-                "owner_name": nftOwner, // Done
-                "last_transfer_date": nftLastTransferDate, // Done
-                "purchase_value": "$0" // Done
+    if(erc721compliant) {
+        obj = {
+            "nft": {
+                "id": 1, // Done
+                "name": nftName, // Done
+                "created_by": nftMinter, // Done
+                "contract_address": collectionAddress, // Done
+                "token_id": id, // Done
+                "chain_id": 1, // Done
+                "mint_timestamp": nftMintDate, // Done
+                "token_type": contractType, // Done
+                "edition_name": 0, // Done
+                "collection_size": 0, // Done
+                "image_url": metaImgAvailable, // Done
+                "owner": {
+                    "owner_name": nftOwner, // Done
+                    "last_transfer_date": nftLastTransferDate, // Done
+                    "purchase_value": "$0" // Done
+                }
             }
         }
+    } else if(erc1155compliant) {
+        obj = {
+            "nft": {
+                "id": 1, // Done
+                "name": nftName, // Done
+                "contract_address": collectionAddress, // Done
+                "token_id": id, // Done
+                "chain_id": 1, // Done
+                "token_type": contractType, // Done
+                "edition_name": 0, // Done
+                "collection_size": 0, // Done
+                "image_url": metaImgAvailable, // Done
+            }
+        }
+    } else {
+        obj = {}
     }
 
     console.log(obj);
  
 }
 
-getNftInfoByCollectionAndId("0x4b61413d4392c806e6d0ff5ee91e6073c21d6430", 11);
+getNftInfoByCollectionAndId("0xA6A5eC7b1B8A34Ff2dcb2926b7c78f52A5ce3b90", 1);
