88d1cab2208feb7375cb26fe1d9c28c84e7a0f94
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2022-05-23 18:00:01 -0300

    Create test.js

---
+++ ERC721/test.js
@@ -0,0 +1,26 @@
+const apiKey = "KXX4T9HFVXGFZ5ZV1B3MXXG7RSSKWRD3DC"
+const api = require('etherscan-api').init(apiKey);
+const Web3 = require("web3");
+const ethNetwork = 'https://mainnet.infura.io/v3/d7f30da03a734567a801120b36cc7f6a';
+const web3 = new Web3(new Web3.providers.HttpProvider(ethNetwork));
+
+let nftTxList;
+
+async function main(collectionAddress, id) {
+
+    await api.log.getLogs(collectionAddress, 0, 99999999, undefined, "AND", undefined, "AND", undefined, "AND", web3.utils.padLeft(web3.utils.numberToHex(id), 64))
+    .then(json => {
+        let txs = {};
+
+        for(const tx of json["result"]) {
+            console.log(tx);
+
+        }
+
+        nftTxList = json["result"];
+    })
+    .catch(err => console.log("Nft Txs Query Error: " + err))
+
+}
+
+main("0xbCe3781ae7Ca1a5e050Bd9C4c77369867eBc307e", 9957);
